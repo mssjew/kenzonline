e241e2600b88d58d1fb2fb2b545968eeaf1cd8bd
hmzsjd <github@hamzasajid.com>
2022-10-12 12:11:27 +0300

    .

--- index.js
+++ index.js
@@ -163,6 +163,7 @@ function listMakerSell(list, content, idx) {
     positionDate.textContent = "";
   } else if (days==0) {
     positionDate.textContent = '\xa0\xa0\xa0\xa0' + content[0] + '\xa0\xa0\xa0' + "TODAY";
+    positionDate.classList.add("yellowB");
   } else {
     positionDate.textContent = days == 1 ? '\xa0\xa0\xa0\xa0' + content[0] + '\xa0\xa0\xa0' + "YESTERDAY" : '\xa0\xa0\xa0\xa0' + content[0] + '\xa0\xa0\xa0' + days + " days";
   }
@@ -220,6 +221,7 @@ function listMakerBuy(list, content, idx) {
     positionDate.textContent = "";
   } else if (days==0) {
     positionDate.textContent = '\xa0\xa0\xa0\xa0' + content[1] + '\xa0\xa0\xa0' + "TODAY";
+    positionDate.classList.add("yellowB");
   } else {
     positionDate.textContent = days == 1 ? '\xa0\xa0\xa0\xa0' + content[1] + '\xa0\xa0\xa0' + "YESTERDAY" : '\xa0\xa0\xa0\xa0' + content[1] + '\xa0\xa0\xa0' + days + " days";
   }
